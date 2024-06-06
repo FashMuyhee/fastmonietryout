ae8bc0253de728f36be30c12ae546b4195aa6564
Fash Muyhee <fashmuyidavid@gmail.com>
2024-06-06 16:27:13 +0100

    add babel module-resolver support

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [["module-resolver", {
+    root: ["."],
+    alias: {
+      'components': './src/components',
+      'utils': './src/utils',
+      'assets': './src/assets',
+      'modules': './src/modules',
+      'hooks': './src/hooks',
+      'contexts': './src/contexts',
+      'routes': './src/routes',
+      'configs': './src/configs',
+    }
+  }]]
 };
